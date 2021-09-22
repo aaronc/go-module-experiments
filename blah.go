ad19e26f03a12ec1b3a7219025b1ba8351aee2b4
Aaron Craelius <aaronc@users.noreply.github.com>
2021-09-22 11:25:32 -0400

    blah

--- blah.go
+++ blah.go
@@ -1,5 +1,15 @@
 package go_module_experiments
 
-//import "github.com/aaronc/go-module-experiments/foo/v1"
+import "github.com/aaronc/go-module-experiments/foo/v1beta1"
+import foo_v2 "github.com/aaronc/go-module-experiments/foo/v2"
+import "github.com/aaronc/go-module-experiments/bar"
+import bar_v2 "github.com/aaronc/go-module-experiments/bar/v2"
 //import "github.com/aaronc/go-module-experiments/foo/v1beta1"
 //import "github.com/aaronc/go-module-experiments/foo/v2"
+
+type Test struct {
+	Bar bar.Bar
+	Bar2 bar_v2.Bar
+	FooV1Beta1 v1beta1.Foo
+	Foo2 foo_v2.Foo
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/aaronc/go-module-experiments
 go 1.17
 
 require (
+	github.com/aaronc/go-module-experiments/bar v0.0.0-20210922152038-5c6ba562596c // indirect
+	github.com/aaronc/go-module-experiments/bar/v2 v2.0.0-20210922152038-5c6ba562596c // indirect
 	github.com/aaronc/go-module-experiments/foo/v1beta1 v0.0.0-20210922151237-c9f337f589f5 // indirect
 	github.com/aaronc/go-module-experiments/foo/v2 v2.0.0-20210922151237-c9f337f589f5 // indirect
 )
